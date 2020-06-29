8e289e16522ae50ccef6e41fbf41cf2d584f351c
iskandarherputra <iskandarherputra@gmail.com>
2020-06-29 11:45:32 +0700

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Iskandar Herputra Wahidiyat on 29/06/20.
+//  Copyright © 2020 Iskandar Herputra Wahidiyat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
